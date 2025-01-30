e1dc70e9f50ed6aa289cac2eff10c0f3dc07a7bb
Omar DevSec <omarsecure30@gmail.com>
2025-01-30 12:31:54 -0800

    Completed Navigation Bar design and animations, Next will work on vibrant color scheme

--- back.js
+++ back.js
@@ -7,3 +7,23 @@ const typed = new Typed(".auto-type",{
     loop: true
   })
   
+
+  /*=====SHOW MENU======*/
+
+  const navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close')
+
+  /*=====Menu show======*/
+  if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+  }
+
+  /*=====Menu Hidden======*/
+  if(navClose){
+      navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+      })
+  }
