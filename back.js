6feeb215d8b24a860d50e96e311a99f8b98b8c61
OmarDevSec <omarsecure30@gmail.com>
2025-03-16 16:33:57 -0700

    Feature: Adjusted about page dynamic typing content

--- back.js
+++ back.js
@@ -1,7 +1,7 @@
 //the typing animation JS code, adjust speed and words displayed-->
 
 const typed = new Typed(".auto-type",{
-    strings: ["Omar","a CyberSecurity Administrator","a Programmer"],
+    strings: ["Omar","a Cybersecurity Admin","a Programmer"],
     typeSpeed: 100,
     backSpeed: 100,
     loop: true
